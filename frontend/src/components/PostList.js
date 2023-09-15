c5c9fe493a98e286480083bcc322fd674f047b81
emmanyouwell <emingala02@gmail.com>
2023-09-15 09:26:30 +0800

    Link title to single post

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Link } from '@mui/material';
 const PostList = ({ post }) => {
 
     return (
         <Container maxWidth="sm">
-
+            <Link href={`/posts/${post.id}`} underline="hover">
             <Typography variant="h1" gutterBottom sx={{ color: 'red' }} component="h2">
                 {post.title}
             </Typography>
+            </Link>
 
             <Typography variant="body1">{post.content}</Typography>
 
