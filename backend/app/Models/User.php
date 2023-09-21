40c02bd9b8aacaffe11990806082e0ba25a5447d
emmanyouwell <emingala02@gmail.com>
2023-09-21 15:17:48 +0800

    API AUTH LOGIN ROUTE

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -43,15 +43,28 @@ class User extends Authenticatable implements JWTSubject
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     public function posts(){
         return $this->hasMany("App\Http\Models\Post");
     }
 
-    public function getJWTIdetifier(){
-        return $this->getKey();
-    }
-    public function getJWTCustomClaims(){
-        return [];
-    }
+    
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,9 @@ use App\Http\Controllers\PostController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware(['api'])->group(function(){
+    Route::post('/login', [AuthController::class, 'login']);
 });
 Route::resource('posts', PostController::class);
 
